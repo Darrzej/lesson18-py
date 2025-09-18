4d99b2b012e9aa0322321eae4f2a0d4569e78d54
Darrzej <kastratidarsej@gmail.com>
2025-09-18 20:36:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,37 @@ col2.metric("Unique title", unique_title)
 col3.metric("Average rating", average_rating)
 col4.metric("Average price", average_price)
 
+st.write(books_df.head())
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.subheader("Top 10 book titles!")
+    top_title = books_df['Name'].value_counts().head(10)
+    st.bar_chart(top_title)
+
+with col2:
+    st.subheader("Top 10 book authors!")
+    top_authors = books_df['Author'].value_counts().head(10)
+    st.bar_chart(top_authors)  
+
+st.subheader("genre Distribution")
+fig = px.pie(books_df, names="Genre", title='Most liked Genre 2009-2022', color="Genre",
+             color_discrete_sequence=px.colors.sequential.Plasma)
+st.plotly_chart(fig) 
+
+st.subheader("Top 15 Authors")
+top_authors = books_df['Author'].value_counts().head(15).reset_index()
+top_authors.columns=['Author','Count']
+
+figg = px.bar(top_authors, x='Count', y='Author', orientation='h',
+              title='Top 15 Authors',
+              labels={'Count': 'Counts of Books Published','Author':'Author'},
+              color='Count', color_continuous_scale=px.colors.sequential.Plasma
+              )
+
+st.plotly_chart(figg)
+
 
 # df = pd.DataFrame({
 #     'Name': ['Hivzi', 'Klajdi', 'Alphonso'],
